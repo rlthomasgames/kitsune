069d4d4a5b2619a6c93e0ea981c118bb5d56dbe6
Rhys Thomas <rlthomasgames@gmail.com>
2022-05-19 02:55:13 +0100

    modifications to package

---
+++ kitsune-wrapper-library/src/hello-world.ts
@@ -0,0 +1,10 @@
+export function sayHello() {
+  console.log('hi')
+}
+export function sayGoodbye() {
+  console.log('goodbye')
+}
+
+export function working() {
+  console.log('example working in web ??');
+}
