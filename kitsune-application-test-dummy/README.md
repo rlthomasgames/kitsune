361942c742daab36ede5ad0b9351c0acc6c9b2f9
Rhys Thomas <rlthomasgames@gmail.com>
2022-05-27 01:43:48 +0100

    added a testing and development skeleton kitsune application project

---
+++ kitsune-application-test-dummy/README.md
@@ -0,0 +1,9 @@
+<h1><img height="64px" src="./test-dummy.ico" width="64px"/> Kitsune Application Test Dummy</h1>
+Kitsune Test Dummy Appllication - more info soon.
+
+# Build Commands
+```
+    npm i
+    npm run build
+```
+this will build and output the testing and development application.
---
+++ kitsune-application-test-dummy/webpack.config.production.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
+
+/*============================================*/
+/*  MUST ADD ENTRY HERE FOR EACH NEW MODULE   */
+/*============================================*/
+const moduleEntries = {
+    index: {
+        import: './index.ts'
+    }
+};
+module.exports = {
+    mode: 'development',
+    entry: moduleEntries,
+    devtool: 'inline-source-map',
+    watch: false,
+    devServer: {
+        static: './dist',
+    },
+    externalsPresets: { node: false }, // in order to ignore built-in extensions like path, fs, etc.
+    externals: [], // in order to ignore all extensions in node_modules folder
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+        ],
+    },
+    plugins: [
+    ],
+    output: {
+        clean: true,
+        publicPath: '',
+        filename: (pathData) => {
+            switch (pathData.chunk.name) {
+                case 'index':
+                    return 'index.js';
+                    break;
+                default:
+                    return '[name].bundle.js';
+            }
+        },
+        path: path.resolve(__dirname, '../../kitsune-wrapper/dist/applications/test_dummy/')
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
+    context: path.resolve(__dirname, './src/')
+};
+const packageMinified = () => {
+    const entries = Object.keys(moduleEntries);
+    const includedMinified = ['index.js'];
+    entries.forEach((name) => {
+        if (String(moduleEntries[name].import).includes('extensions/')) {
+            includedMinified.push(`../../kitsune-wrapper/dist/applications/test_dummy/index.js`);
+        }
+    });
+    const terserOptions = { include: includedMinified };
+    module.exports.optimization.minimizer.push(new TerserPlugin(terserOptions));
+};
+packageMinified();
