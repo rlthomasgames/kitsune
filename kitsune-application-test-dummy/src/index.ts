d2eb26ac2bb1e2eb22db3c89955a6fbbe1d81445
Rhys Thomas <rlthomasgames@gmail.com>
2022-05-29 02:58:13 +0100

    initial stages on a test application

--- kitsune-application-test-dummy/src/index.ts
+++ kitsune-application-test-dummy/src/index.ts
@@ -1,5 +1,15 @@
-export class KTD {
+import {BitmapText} from "pixi.js";
+import {IsolatedRuntime} from "isolated-runtime";
 
+export class KTD {
+    runtimeInstance = new IsolatedRuntime({});
+
+    constructor() {
+        this.runtimeInstance.run({
+            args: [], file: "", funcName: "", root: ""
+
+        })
+    }
 }
 
-console.log('hello?');
+console.log('hello?', BitmapText);
