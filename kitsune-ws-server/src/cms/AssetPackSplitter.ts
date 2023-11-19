d7200b97560dff545c2565e15e6fb675f97ec385
Rhys Thomas <rlthomasgames@gmail.com>
2023-11-19 06:10:02 +0000

    WIP test now creates packet files

--- kitsune-ws-server/src/cms/AssetPackSplitter.ts
+++ kitsune-ws-server/src/cms/AssetPackSplitter.ts
@@ -1,4 +1,6 @@
- import * as fs from "fs";
+import * as fs from "fs";
+import sha256 = require("sha256");
+import {strToU8} from "fflate";
 
 /*========================================
 
@@ -14,59 +16,105 @@
   O: uniqueID<string>, amount of packets<number>
 
 ======================================= */
-const RUN_TEST :boolean = true;
-const testGzip = './cms/split_pack_test/81d96cd2ae3b0418c03755031305a5ca612acf549eac4e672da9b18f18a4f1f2.gz'
+const RUN_TEST: boolean = true;
+const testGzip = '81d96cd2ae3b0418c03755031305a5ca612acf549eac4e672da9b18f18a4f1f2'
 
 export class AssetPackSplitter {
-     private splitPartSize = 500;
+    private assetsLocationPrefix = './cms/uploaded/';
+    private assetsSuffix = '.gz';
+    private assetsPacketsPrefix: string = './cms/packets/';
+    private splitPartSize = 500;
+
     constructor(urlGZIP: string) {
-        if(RUN_TEST) {
-            urlGZIP = testGzip //test
-            const manifest = this.splitAssets(urlGZIP);
-            manifest.then((returnedManifest) => {
-                console.log('manifest', manifest);
+        if (RUN_TEST) {
+            urlGZIP = `${this.assetsLocationPrefix}${testGzip}${this.assetsSuffix}` //test
+            this.splitWithManifest(urlGZIP).then((value) => {
+                return value;
             });
         }
     }
 
-    async splitAssets(url:string) {
+    async splitWithManifest(url) {
+        const manifest = this.splitAssets(url);
+        return await manifest.then((returnedManifest) => {
+            console.log('manifest', returnedManifest);
+            return returnedManifest;
+        });
+    }
+
+    async splitAssets(url: string) {
         const fileSize = await this.getFileSize(url);
         const numParts = this.numParts(fileSize);
-        return await new Promise<IAssetPacksManifest>(async (resolve, reject) => {
-            fs.open(url, (err, fd)=>{
-                fs.read(fd, (err2, bytesRead, buffer)=>{
+        return await new Promise<IAssetPacksManifest>((resolve, reject) => {
+            const packets: Array<Array<string>> = [];
+            fs.open(url, (err, fd) => {
+                fs.read(fd, (err2, bytesRead, buffer) => {
                     const wholeString = buffer.toString();
-                    const array = wholeString.split('');
+                    const array: Array<string> = wholeString.split('');
                     console.log('buffer = ', buffer, 'byte length = ', buffer.byteLength, 'array length = ', array.length, bytesRead);
                     console.log(array);
-                    for(let i = 0; i < numParts; i++){
-                        let part = [];
-                        for( let j = 0; j < this.splitPartSize; j++) {
+                    for (let i = 0; i < numParts; i++) {
+                        let part: Array<string> = [];
+                        for (let j = 0; j < this.splitPartSize; j++) {
                             part.push(array[j]);
                         }
+                        packets.push(part);
                         console.log('part ', i, ' = ', part);
                         array.splice(0, this.splitPartSize);
                     }
                 })
+                fs.close(fd);
+            })
+            console.log('all packets', packets);
+            const paths: Array<string> = [];
+            const uniqueID = sha256(url, {asString: true});
+            fs.opendir(this.assetsPacketsPrefix, (err, dir) => {
+                console.log('open dir', this.assetsPacketsPrefix, err, dir);
+                if (!err) {
+                    fs.mkdir(`${dir.path}${uniqueID}`, undefined, (err, path) => {
+                        console.log(`completed mkdir with`, err, path);
+                        packets.forEach((packet: Array<string>, index) => {
+                            const packetBuffer = Buffer.from(strToU8(packet.join('')) as Uint8Array);
+                            const packetFileName = `${this.assetsPacketsPrefix}${uniqueID}/${index}`;
+                            fs.promises.writeFile(`${packetFileName}`, packetBuffer, {
+                                flag: 'w',
+                                encoding: 'binary'
+                            }).then((value) => {
+                                // paths.push(`${packetFileName}`);
+                            });
+                            paths.push(`${packetFileName}`);
+                        });
+                        console.log('manifest should be ', {
+                            uniqueID,
+                            pathsToParts: paths
+                        });
+                    })
+                }
+                dir.close(() => {
+                    resolve({
+                        uniqueID,
+                        pathsToParts: paths
+                    });
+                });
             })
         });
     }
 
-    numParts(size:number){
+    numParts(size: number) {
         const parts = Math.ceil(size / this.splitPartSize);
-        if(parts < 1) {
+        if (parts < 1) {
             return 1;
         }
         return parts;
     }
 
-    async getFileSize(url:string) {
+    async getFileSize(url: string) {
         const stats = await fs.promises.stat(url);
         return stats.size;
     }
 }
 
 export interface IAssetPacksManifest {
-    uniqueID:string;
-    pathsToParts:string[];
+    uniqueID: string;
+    pathsToParts: string[];
 }
