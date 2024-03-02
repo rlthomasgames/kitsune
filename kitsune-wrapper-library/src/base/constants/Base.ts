34865b258e370e315b36d51448f13d2b392f4c85
Rhys Thomas <rlthomasgames@gmail.com>
2024-03-02 20:36:23 +0000

    fixing lib

--- kitsune-wrapper-library/src/base/constants/Base.ts
+++ kitsune-wrapper-library/src/base/constants/Base.ts
@@ -2,14 +2,4 @@ export const TYPES = {
     FetchConfig: Symbol.for("FetchConfig"),
     Command: Symbol.for("Command"),
     LoadModule: Symbol.for("LoadModule"),
-    InjectionTestFetchConfig: Symbol.for("InjectionTest"),
-    STENCIL_BACK_PASS_DEPTH_PASS: Symbol.for("InjectionTest"),
-    Socket: Symbol.for("KSockService"),
-    AssetDataVendor: Symbol.for("AssetDataVendor")
-
-    /*,
-     these two should now get loaded in the same way three or pixi is loaded
-
-
-    */
 };
